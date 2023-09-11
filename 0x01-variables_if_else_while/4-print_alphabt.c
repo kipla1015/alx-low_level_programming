e6d7e5229ab37c4e1afce1cb0181a26039fc202f
kipla <derrickkiget@gmail.com>
2023-09-10 17:27:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+   * main - this is the function of the code
+   * Return: 0
+   */
+
+int main(void)
+{
+char i, e, q;
+e = 'e';
+q = 'q';
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+
+if (i != e && i != q)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
