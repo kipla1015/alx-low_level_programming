0d0e70799c78fe2bce63caea79e57d71e06646c4
kipla <derrickkiget@gmail.com>
2023-10-04 03:22:29 -0700

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,8 @@
    * Return: always 0
    */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv __attribute__((unused)))
 {
-	(void) argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
